a0e928cb0c2941b041b83774d3125c8998295246
César N <ceesar90@gmail.com>
2019-07-04 23:31:11 -0300

    Fix empty route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 import Home from './components/Home';
 import SearchResults from './components/SearchResult';
@@ -9,12 +9,18 @@ import './assets/vendor/grid.min.css';
 import './App.css';
 
 function App() {
+  const RedirectToMain = _ => {
+    return (
+        <Redirect to="/" />
+    );
+}
   return (
     <BrowserRouter>
       <div className="container">
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/search-results/:slug" component={SearchResults} />
+          <Route exact path="/search-results" component={RedirectToMain} />
         </Switch>
       </div>
     </BrowserRouter>
